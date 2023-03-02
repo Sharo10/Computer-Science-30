17680d680d9bbaf0181d89707b78288e020ea71c
Sharo10 <working.omarhos@gmail.com>
2023-03-02 15:14:00 -0600

    First Demo

---
+++ MovingRectangle/sketch.js
@@ -0,0 +1,16 @@
+// First Day of VS Code
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  rect(mouseX, 100, 200, 50);
+}
