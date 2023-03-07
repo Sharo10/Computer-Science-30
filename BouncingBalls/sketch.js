d008567ddd0739d70806b34331b4772dface5639
Sharo10 <working.omarhos@gmail.com>
2023-03-07 14:54:44 -0600

    Bouncing balls

---
+++ BouncingBalls/sketch.js
@@ -0,0 +1,58 @@
+// Bouncing balls
+// Ahmed Omar
+// 3/6/2023
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+// Bouncing Balls Demo
+// Using Arrays and Object Notation
+
+let shapes = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  spawnBall(width/2, height/2); 
+}
+
+function draw() {
+  background(220);
+  moveShapes();
+  displayShapes();
+}
+
+function mousePressed() {
+  spawnBall(mouseX, mouseY);
+}
+
+function moveShapes() {
+  for (let i=0; i<shapes.length; i++) {
+    shapes[i].x += shapes[i].dx;
+    shapes[i].y += shapes[i].dy;
+    if (shapes[i].x <= 0 || shapes[i].y >= height || shapes[i].y <= height ){
+      shapes[i].x -= shapes[i].dx;
+      shapes[i].y -= shapes[i].dy;
+
+    }
+  }
+}
+
+function displayShapes() {
+  for (let i=0; i<shapes.length; i++) {
+    fill(shapes[i].theColor);
+    circle(shapes[i].x, shapes[i].y, shapes[i].diameter);
+  }
+}
+
+function spawnBall(tempX, tempY) {
+  let newBall = {
+    x: tempX,
+    y: tempY,
+    dx: random(-5, 5),
+    dy: random(-5, 5),
+    diameter: random(25, 100),
+    theColor: color(random(255), random(255), random(255))
+  };
+  shapes.push(newBall);
+}
