144091d231d1b61bd74a0ab0f3f997f1363cd6c6
Sharo10 <working.omarhos@gmail.com>
2023-03-14 14:19:06 -0600

    Test

---
+++ Arrays assignment/sketch.js
@@ -0,0 +1,15 @@
+// Arrays and object notation assignment
+// Ahmed Omar
+// 3/8/2023
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
