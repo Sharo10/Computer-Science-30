28c48f1bbfb1289dc2f7b376dc68ed230cfcf764
Sharo10 <126823561+Sharo10@users.noreply.github.com>
2023-04-05 05:20:55 -0600

    Update sketch.js

--- 2dArraysProject/sketch.js
+++ 2dArraysProject/sketch.js
@@ -1,257 +1,150 @@
-// Project Title
-// Ahmed Omar
-// 3/28/2023
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// // Conweeper
+// // Ahmed Omar
+// // 3/28/2023
+// //
+// // Extra for Experts:
+// // - describe what you did to take this project "above and beyond"
 
 
-let wordList = ["apple", "banana", "cherry", "orange", "grape"]; 
+let gridSize = 20;
+let cellSize;
+let grid;
+let bombs = [];
+let gameOver = false;
 
-let secretWord = ""; 
+function setup() {
+  createCanvas(400, 400);
+  cellSize = width / gridSize;
+  grid = createEmptyGrid(gridSize, gridSize);
+  generateBombs(50);
+}
 
-let guessedLetters = []; 
+function draw() {
+  background(255);
+  drawGrid(grid);
+  if (gameOver) {
+    drawBombs();
+  }
+}
 
-let wrongGuesses = 0; 
+function mousePressed() {
+  if (!gameOver) {
+    let x = floor(mouseX / cellSize);
+    let y = floor(mouseY / cellSize);
 
-let maxWrongGuesses = 6; 
+    if (grid[y][x] === 0) {
+      if (isBomb(x, y)) {
+        gameOver = true;
+        return;
+      }
+      grid[y][x] = 1;
+      updateNeighbors(x, y);
+    }
+  }
+}
 
-let gameWon = false; 
+function createEmptyGrid(rows, cols) {
+  let emptyGrid = new Array(rows);
+  for (let i = 0; i < rows; i++) {
+    emptyGrid[i] = new Array(cols).fill(0);
+  }
+  return emptyGrid;
+}
 
-let gameLost = false; 
+function generateBombs(numBombs) {
+  for (let i = 0; i < numBombs; i++) {
+    let x = floor(random(gridSize));
+    let y = floor(random(gridSize));
+    bombs.push({ x: x, y: y });
+  }
+}
 
-let letterSize = 32; 
+function drawGrid(grid) {
+  for (let y = 0; y < gridSize; y++) {
+    for (let x = 0; x < gridSize; x++) {
+      if (grid[y][x] === 1) {
+        fill(0);
+      } else {
+        fill(255);
+      }
+      stroke(200);
+      rect(x * cellSize, y * cellSize, cellSize, cellSize);
+    }
+  }
+}
 
-let buttonWidth = 60; 
+function drawBombs() {
+  fill(255, 0, 0);
+  for (let bomb of bombs) {
+    rect(bomb.x * cellSize, bomb.y * cellSize, cellSize, cellSize);
+  }
+}
 
+function isBomb(x, y) {
+  for (let bomb of bombs) {
+    if (bomb.x === x && bomb.y === y) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function updateNeighbors(x, y) {
+  let neighbors = [
+    [-1, -1], [0, -1], [1, -1],
+    [-1, 0],           [1, 0],
+    [-1, 1],  [0, 1],  [1, 1]
+  ];
+
+  for (let [dx, dy] of neighbors) {
+    let newX = x + dx;
+    let newY = y + dy;
+
+    if (newX >= 0 && newY >= 0 && newX < gridSize && newY < gridSize) {
+      if (grid[newY][newX] === 0 && !isBomb(newX, newY)) {
+        grid[newY][newX] = 1;
+
+        // Conway's Game of Life rule: If a cell has exactly 3 neighbors, turn it on
+        let count = countNeighbors(newX, newY);
+        if (count === 3) {
+          grid[newY][newX] = 1;
+        } else {
+          grid[newY][newX] = 0;
+        }
+      }
+    }
+  }
+}
+
+function countNeighbors(x, y) {
+  let count = 0;
+  let neighbors = [
+    [-1, -1], [0, -1], [1, -1],
+    [-1, 0], [1, 0],
+    [-1, 1], [0, 1], [1, 1]
+    ];
+    
+    for (let [dx, dy] of neighbors) {
+    let newX = x + dx;
+    let newY = y + dy;
+    if (newX >= 0 && newY >= 0 && newX < gridSize && newY < gridSize) {
+      if (grid[newY][newX] === 1) {
+        count++;
+      }
+    }
+  }
+  return count;
+  }
   
-
-function setup() { 
-
-  createCanvas(800, 600); 
-
-  secretWord = random(wordList); 
-
-  for (let i = 0; i < secretWord.length; i++) { 
-
-    guessedLetters.push("_"); 
-
-  } 
-
-  textSize(letterSize); 
-
-  textAlign(CENTER, CENTER); 
-
-} 
-
+  function keyPressed() {
+  if (key === 'r' || key === 'R') {
+  resetGame();
+  }
+  }
   
-
-function draw() { 
-
-  background(255); 
-
-  drawGallows(); 
-
-  drawHangman(); 
-
-  drawSecretWord(); 
-
-  drawAlphabet(); 
-
-  checkGameState(); 
-
-} 
-
-  
-
-function drawGallows() { 
-
-  stroke(0); 
-
-  strokeWeight(4); 
-
-  line(100, 450, 200, 450); 
-
-  line(150, 450, 150, 100); 
-
-  line(150, 100, 300, 100); 
-
-  line(300, 100, 300, 150); 
-
-} 
-
-  
-
-function drawHangman() { 
-
-  if (wrongGuesses > 0) { 
-
-    ellipse(300, 200, 100, 100); // Head 
-
-  } 
-
-  if (wrongGuesses > 1) { 
-
-    line(300, 250, 300, 350); // Body 
-
-  } 
-
-  if (wrongGuesses > 2) { 
-
-    line(300, 275, 250, 225); // Left arm 
-
-  } 
-
-  if (wrongGuesses > 3) { 
-
-    line(300, 275, 350, 225); // Right arm 
-
-  } 
-
-  if (wrongGuesses > 4) { 
-
-    line(300, 350, 250, 400); // Left leg 
-
-  } 
-
-  if (wrongGuesses > 5) { 
-
-    line(300, 350, 350, 400); // Right leg 
-
-  } 
-
-} 
-
-  
-
-function drawSecretWord() { 
-
-  fill(0); 
-
-  for (let i = 0; i < guessedLetters.length; i++) { 
-
-    text(guessedLetters[i], 400 + i * letterSize, height / 2); 
-
-  } 
-
-} 
-
-  
-
-function drawAlphabet() { 
-
-  for (let i = 0; i < 26; i++) { 
-
-    let x = 400 + (i % 13) * buttonWidth; 
-
-    let y = (i < 13) ? 500 : 570; 
-
-    let letter = String.fromCharCode(65 + i); 
-
-    fill(200); 
-
-    rect(x, y, buttonWidth, buttonWidth, 8); 
-
-    fill(0); 
-
-    text(letter, x + buttonWidth / 2, y + buttonWidth /2); 
-
-  } 
-
-} 
-
-  
-
-function mouseClicked() { 
-
-  if (!gameWon && !gameLost) { 
-
-    let clickedLetter = getClickedLetter(); 
-
-    if (clickedLetter) { 
-
-      makeGuess(clickedLetter.toLowerCase()); 
-
-    } 
-
-  } 
-
-} 
-
-  
-
-function getClickedLetter() { 
-
-  for (let i = 0; i < 26; i++) { 
-
-    let x = 400 + (i % 13) * buttonWidth; 
-
-    let y = (i < 13) ? 500 : 570; 
-
-    if (mouseX > x && mouseX < x + buttonWidth && mouseY > y && mouseY < y + buttonWidth) { 
-
-      return String.fromCharCode(65 + i); 
-
-    } 
-
-  } 
-
-  return null; 
-
-} 
-
-  
-
-function makeGuess(letter) { 
-
-  if (secretWord.includes(letter)) { 
-
-    for (let i = 0; i < secretWord.length; i++) { 
-
-      if (secretWord[i] === letter) { 
-
-        guessedLetters[i] = letter; 
-
-      } 
-
-    } 
-
-  } else { 
-
-    wrongGuesses++; 
-
-  } 
-
-} 
-
-  
-
-function checkGameState() { 
-
-  if (guessedLetters.join("") === secretWord) { 
-
-    gameWon = true; 
-
-    displayResult("You won!"); 
-
-  } else if (wrongGuesses >= maxWrongGuesses) { 
-
-    gameLost = true; 
-
-    displayResult("You lost! The word was: " + secretWord); 
-
-  } 
-
-} 
-
-  
-
-function displayResult(message) { 
-
-  textSize(48); 
-
-  fill(255, 0, 0); 
-
-  text(message, width / 2, 100); 
-
-} 
+  function resetGame() {
+  gameOver = false;
+  grid = createEmptyGrid(gridSize, gridSize);
+  bombs = [];
+  generateBombs(50);
+  }    
